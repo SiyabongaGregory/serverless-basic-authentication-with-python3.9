10622b182f294cab09c7770c37f9e8245912a614
Siyabonga Gregory <121162357+SiyabongaGregory@users.noreply.github.com>
2023-04-24 08:21:19 +0200

    Updated runtime
    Since the runtime parameter of python3.6 is no longer supported for creating or updating AWS Lambda functions. AWS recommend we use the new runtime (python3.9) while creating or updating functions.

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
-# serverless-basic-authentication-with-python3.9
+## Serverless Basic Authentication (http basic auth) With Python3.9
+
+Sometimes you need to integrate your api with some outside system, and you are not capable of setting up custom headers with keys. Almost all systems support Basic Authentication out of the box though. Which is where this plugin comes in.
+
+This plugin will install a custom authenticator for the functions you specify as being private, and use the API Keys (so no user management required) as http basic username and password.
+
+When using this plugin, you can use both the `x-api-key` header, or the `Authorization` header for authentication.
+
+## Installation
+
+`npm install serverless-basic-authentication-with-python3.9`
+
+Add the plugin to your settings:
+
+```
+plugins:
+  - serverless-basic-authentication-with-python3.9
+```
+
+And give access so that the plugin can check the api keys:
+
+```
+provider:
+  name: aws
+  ...
+  iamRoleStatements:
+    ...
+    - Effect: Allow
+      Action:
+        - apigateway:GET
+      Resource: "*"
+```
+
+## Usage
+
+Add some keys to your service:
+
+```
+provider:
+  name: aws
+  ...
+  apiKeys:
+    - foobar
+    - platypus
+```
+
+For each function that responds to `http` events and is marked as `private: true`, the custom authenticator will be inserted, like so:
+
+```
+functions:
+  foobar:
+    handler: handler.foobar
+    events:
+      - http:
+          path: foo/bar
+          method: get
+          private: true
+```
+
+To send the correct header so that browsers will prompt for username and password, add a `GatewayResponse` to the `resources`:
+
+```
+resources:
+  Resources:
+    GatewayResponse:
+      Type: 'AWS::ApiGateway::GatewayResponse'
+      Properties:
+        ResponseParameters:
+          gatewayresponse.header.WWW-Authenticate: "'Basic'"
+        ResponseType: UNAUTHORIZED
+        RestApiId:
+          Ref: 'ApiGatewayRestApi'
+        StatusCode: '401'
+```
+
+If you are whitelisting files to be packaged, ensure you add `basic_auth.py` to the list otherwise the authorizer will fail:
+
+```
+package:
+  exclude:
+    - "./**/**"
+  include:
+    - basic_auth.py
+```
+
+**Note:** The plugin checks if a custom authorizer is already set. So if you provide a custom authorizer it will not override your custom authorizer.
+
+After deploying, you can call the endpoint with a basic auth username/password:
+
+```
+curl -u [key-name]:[key-value] https://abckudzdef.execute-api.eu-west-1.amazonaws.com/dev/foo/bar
+"yay"
+```
+
+## How does this work?
+
+In Api Gateway, the custom authorizer function can also be used to supply the api key for a request. In this case, we lookup the api key on the fly through the api-gateway api, and check if the key matches. If so, we tell Api Gateway to use that key for handling the calls.
+
+PR's are appreciated!
---
+++ index.js
@@ -0,0 +1,156 @@
+const fs = require("fs");
+const chalk = require("chalk");
+const path = require("path");
+
+module.exports = class SetupBasicAuthentication {
+  constructor(serverless, options) {
+    this.options = options;
+    this.serverless = serverless;
+
+    // add the basic authentication function to the functions as soon as possible
+    this.injectBasicAuthFunction(serverless);
+
+    this.hooks = {
+      "before:package:initialize": this.addAuthorizer.bind(this),
+      "after:package:createDeploymentArtifacts":
+        this.removeAuthorizer.bind(this),
+      "before:deploy:deploy": this.configureApiGatewayKeySource.bind(this),
+    };
+  }
+
+  addAuthorizer() {
+    // add our custom authenticator
+    this.addAuthFileToPackage();
+
+    this.addAuthorizerFunctionToPrivateFunctions();
+  }
+
+  removeAuthorizer() {
+    this.serverless.cli.consoleLog(
+      `Basic Authentication: ${chalk.yellow(
+        "Removing Symlink for Basic Authenticator"
+      )}`
+    );
+    fs.unlinkSync(
+      path.normalize(`${this.serverless.config.servicePath}/basic_auth.py`)
+    );
+  }
+
+  addAuthFileToPackage() {
+    if (!this.serverless.package) {
+      this.serverless.package = {};
+    }
+
+    if (!this.serverless.package.include) {
+      this.serverless.package.include = [];
+    }
+
+    this.serverless.cli.consoleLog(
+      `Basic Authentication: ${chalk.yellow(
+        "Adding Symlink for Basic Authenticator"
+      )}`
+    );
+    // @TODO: Make target filename randomized with something, to prevent overriding
+    // any files
+
+    // append our basic_auth.py file to the package
+    this.serverless.package.include.push(
+      path.normalize(`${__dirname}/basic_auth.py`)
+    );
+    try {
+      fs.symlinkSync(
+        path.normalize(`${__dirname}/basic_auth.py`),
+        path.normalize(`${this.serverless.config.servicePath}/basic_auth.py`)
+      );
+    } catch (error) {
+      if (error.errno === -4048 && error.code === "EPERM") {
+        fs.copyFileSync(
+          path.normalize(`${__dirname}/basic_auth.py`),
+          path.normalize(`${this.serverless.config.servicePath}/basic_auth.py`)
+        );
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  injectBasicAuthFunction() {
+    this.serverless.cli.consoleLog(
+      `Basic Authentication: ${chalk.yellow(
+        "Adding function for Basic Authenticator"
+      )}`
+    );
+    const basicAuthenticator = {
+      handler: "basic_auth.basicAuth",
+      runtime: "python3.9",
+    };
+
+    // add the basic authenticator function
+    this.serverless.service.functions.basicAuthenticator = basicAuthenticator;
+  }
+
+  addAuthorizerFunctionToPrivateFunctions() {
+    // for each function which is marked as 'private', set the basic authenticator
+    // if it doesn't have a custom authenticator yet
+    Object.keys(this.serverless.service.functions).forEach((functionName) => {
+      // ignore our own function
+      if (functionName === "basicAuthenticator") {
+        return;
+      }
+
+      // get all function configs
+      const fnctn = this.serverless.service.functions[functionName];
+
+      // check if any of the http events is marked as private, and if that event
+      // also doesn't have a custom authorizer already, apply our authenticator
+      Object.keys(fnctn.events).forEach((fnctnEvent) => {
+        // if http doesn't exist, skip
+        if (
+          !(
+            "http" in
+            this.serverless.service.functions[functionName].events[fnctnEvent]
+          )
+        ) {
+          return;
+        }
+
+        if (
+          this.serverless.service.functions[functionName].events[fnctnEvent]
+            .http != null &&
+          this.serverless.service.functions[functionName].events[fnctnEvent]
+            .http.private === true &&
+          this.serverless.service.functions[functionName].events[fnctnEvent]
+            .http.authorizer == null
+        ) {
+          this.serverless.service.functions[functionName].events[
+            fnctnEvent
+          ].http.authorizer = {
+            name: "basicAuthenticator",
+            identitySource: "", // this is only valid if we set cache ttl to 0
+            resultTtlInSeconds: 0,
+            type: "REQUEST",
+          };
+          this.serverless.cli.consoleLog(
+            `Basic Authentication: ${chalk.yellow(
+              `Enabled for ${functionName}`
+            )}`
+          );
+        }
+      });
+    });
+  }
+
+  configureApiGatewayKeySource() {
+    const template =
+      this.serverless.service.provider.compiledCloudFormationTemplate;
+    if (template.Resources.ApiGatewayRestApi != null) {
+      this.serverless.cli.consoleLog(
+        `Basic Authentication: ${chalk.yellow(
+          "Configuring Api Gateway for Basic Authenticator"
+        )}`
+      );
+      template.Resources.ApiGatewayRestApi.Properties.ApiKeySourceType =
+        "AUTHORIZER";
+    }
+  }
+};
